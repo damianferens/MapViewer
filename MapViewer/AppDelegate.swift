56b5c010065388891a7ab2ccf104f0946149e506
Damian Ferens <damianferens@gmail.com>
2017-03-20 10:05:25 +0100

    Unused code deleted. Ready to send

--- MapViewer/AppDelegate.swift
+++ MapViewer/AppDelegate.swift
@@ -16,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyA9lxg88Ib46SBS9XnPdPXZdgZlXJ1y8qo")
-        // Override point for customization after application launch.
-        if let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.path {
-            print("Documents Directory: " + documentsPath)
-        }
         return true
     }
     
--- MapViewer/DatabaseManager.swift
+++ MapViewer/DatabaseManager.swift
@@ -11,13 +11,11 @@ import CoreData
 class DatabaseManager: NSObject {
     var mainManagedObjectContext: NSManagedObjectContext
     var backgroundManagedObjectContext: NSManagedObjectContext
-
+    
     override init() {
-        // This resource is the same name as your xcdatamodeld contained in your project.
         guard let modelURL = Bundle.main.url(forResource: "Model", withExtension:"momd") else {
             fatalError("Error loading model from bundle")
         }
-        // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
         guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
             fatalError("Error initializing mom from: \(modelURL)")
         }
@@ -29,9 +27,6 @@ class DatabaseManager: NSObject {
         DispatchQueue.global().async() {
             let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
             let docURL = urls[urls.endIndex-1]
-            /* The directory the application uses to store the Core Data store file.
-             This code uses a file named "DataModel.sqlite" in the application's documents directory.
-             */
             let storeURL = docURL.appendingPathComponent("Model")
             do {
                 try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
@@ -43,11 +38,9 @@ class DatabaseManager: NSObject {
     
     func saveSelectedPlace(place: Place) {
         backgroundManagedObjectContext.perform {
-            let entity = NSEntityDescription.entity(forEntityName: "SelectedPlace",
-                                                    in: self.backgroundManagedObjectContext)!
+            let entity = NSEntityDescription.entity(forEntityName: "SelectedPlace", in: self.backgroundManagedObjectContext)!
             
-            let placeToSave = NSManagedObject(entity: entity,
-                                              insertInto: self.backgroundManagedObjectContext)
+            let placeToSave = NSManagedObject(entity: entity, insertInto: self.backgroundManagedObjectContext)
             placeToSave.setValue(place.avatar, forKey: "avatar")
             placeToSave.setValue(place.name, forKey: "name")
             placeToSave.setValue(place.id, forKey: "identifier")
@@ -61,7 +54,7 @@ class DatabaseManager: NSObject {
             }
         }
     }
-
+    
     func allSelectedPlaces(completionHandler: @escaping ([SelectedPlace]) -> ()){
         mainManagedObjectContext.perform {
             let fetchRequest: NSFetchRequest<SelectedPlace> = SelectedPlace.fetchRequest()
